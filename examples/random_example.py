0178bd5f1d143feace7fad1c1a91292d7d4377cd
rezarex <marvin.wekesa@gmail.com>
2023-10-24 08:13:53 +0300

    new

--- examples/random_example.py
+++ examples/random_example.py
@@ -7,7 +7,7 @@ import yaml
 # a great day to die
 
 from elasticsearch.helpers import bulk
-#random issue.....trialmmmmmm
+#random issue.....trial
 def main():
     fields = []
     es = Elasticsearch(["http://localhost:9200"])
