1cba63e3c332829a7888d7a70542329c137e74ce
rezarex <marvin.wekesa@gmail.com>
2023-10-24 08:29:44 +0300

    new

--- examples/random_example.py
+++ examples/random_example.py
@@ -4,7 +4,7 @@ import random
 from elasticsearch import Elasticsearch
 import yaml
 
-# a great day to die
+# untill the end of time
 
 from elasticsearch.helpers import bulk
 #random issue.....trial
