3a7a902d5103b3cbc145a5617d18dd4d7a1ce83f
rezarex <marvin.wekesa@gmail.com>
2023-10-24 13:25:35 +0300

    new

--- examples/random_example.py
+++ examples/random_example.py
@@ -4,7 +4,7 @@ import random
 from elasticsearch import Elasticsearch
 import yaml
 
-# until the end of time....
+# until the end of timejdfhf....
 
 from elasticsearch.helpers import bulk
 def main():
