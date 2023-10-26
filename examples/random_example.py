4bb6689023483cdb080262650b92412e93cb5452
rezarex <marvin.wekesa@gmail.com>
2023-10-26 20:27:50 +0300

    test

--- examples/random_example.py
+++ examples/random_example.py
@@ -3,7 +3,7 @@ import json
 import random
 from elasticsearch import Elasticsearch
 import yaml
-#dskfmlmekjfgljrjjijkfdfgddhfhdfjslfsidofekjflweurhkgjhfjefnknsdjvkj
+#dskfmlmekjfgljrjjijkfdfgddhfhdfjslfsidofekjflweurhkgj
 
 from elasticsearch.helpers import bulk
 def main():
